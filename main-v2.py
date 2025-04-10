de1b7b123a09cc75d272a9036277dc8c36885c41
shlabisa <shlabisa@sarao.ac.za>
2025-04-10 10:56:01 +0200

    Scalled up the clash time by 25, then increased the number of proposals for this week by reducing the buffer then finally increase the number of interations

--- main-v2.py
+++ main-v2.py
@@ -277,7 +277,7 @@ class Timetable:
         numerator = 0
         denominator = 1
 
-        total_non_clash_time = total_time - total_clash_time
+        total_non_clash_time = total_time - min(total_time * 0.5, total_clash_time * 25) # scalling up the clash time to increase it impact
         total_num_scheduled_proposals = total_num_proposals - total_num_unscheduled_proposals
         if total_time > total_num_proposals:
             scaler = total_time / total_num_proposals
@@ -391,7 +391,7 @@ class Timetable:
                             ha='center', va='center', fontsize=10, color='black')
 
                 # Add to legend with index and email
-                legend_key = f'{idx} {proposal.owner_email} {proposal.lst_start_time.strftime("%H:%M:%S")} {proposal.lst_start_end_time.strftime("%H:%M:%S")} {proposal.night_obs} {proposal.avoid_sunrise_sunset}'
+                legend_key = f'{idx} {proposal.owner_email.split("@")[0].split(".")[0]} {proposal.lst_start_time.strftime("%H:%M:%S")} {proposal.lst_start_end_time.strftime("%H:%M:%S")} {proposal.night_obs} {proposal.avoid_sunrise_sunset}'
                 if legend_key not in legend_dict:
                     legend_dict[legend_key] = color
 
@@ -561,13 +561,13 @@ def main():
     for proposal in proposals:
         # Check if the proposal can be scheduled
         if not can_schedule_proposal(proposal):
-            print(proposal.owner_email, proposal.night_obs, proposal.)
+            print("**", proposal.owner_email, proposal.night_obs, proposal.avoid_sunrise_sunset, proposal.lst_start_time, proposal.lst_start_end_time)
             continue  # Skip this proposal if it cannot be scheduled
         if proposal.night_obs or proposal.avoid_sunrise_sunset:
             print(proposal.owner_email)
         
         cumulative_week_duration += proposal.simulated_duration
-        if cumulative_week_duration > total_week_duration * 1.25:
+        if cumulative_week_duration > total_week_duration * 0.85:
             break
 
         # If the proposal is valid, add it to the scheduled proposals
@@ -597,7 +597,7 @@ def main():
     """
 
     print("Generating Timetable using Genetic Algorithim")
-    genetic_algorithm: GeneticAlgorithm = GeneticAlgorithm(10, 1000)
+    genetic_algorithm: GeneticAlgorithm = GeneticAlgorithm(10, 5000)
     best_timetable: Timetable = genetic_algorithm.get_best_fit_timetable()
     best_timetable.plot(filename=f'outputs/week {MIN_DATE.strftime("%m-%d-%Y")} to {MAX_DATE.strftime("%m-%d-%Y")} timetable.png')
     
