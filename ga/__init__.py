f724bf394df6c70bd18a9f303245c10cee1dfb2f
shlabisa <shlabisa@sarao.ac.za>
2025-07-01 14:30:02 +0200

    Imported Schedule and Individual classes

--- ga/__init__.py
+++ ga/__init__.py
@@ -1,4 +1,6 @@
 from .proposal import Proposal
+from .schedule import Schedule
+from .individual import Individual
+from .genetic_algorithim import GeneticAlgorithm
 from .timetable import Timetable
-from .genetic_algorithim import Genetic_Algorithm
 from .utils import *
