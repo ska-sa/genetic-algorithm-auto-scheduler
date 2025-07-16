b4e04b55eedfce6ccd1e7fea9b24d49de6f1fcb3
shlabisa <shlabisa@sarao.ac.za>
2025-07-16 16:08:54 +0200

    Added a redirect from root to user/timetables route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { TimetablesComponent } from './components/timetables/timetables.component';
+import { TimetableDetailsComponent } from './components/timetable-details/timetable-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'user/timetables', pathMatch: 'full'
+    },
+    {
+        path: 'user', component: UserComponent, children: [
+            {
+                path: 'timetables', component: TimetablesComponent, children: [
+                    {
+                        path: ':id/details', component: TimetableDetailsComponent
+                    },
+                ]
+            }
+        ]
+    }
+];
