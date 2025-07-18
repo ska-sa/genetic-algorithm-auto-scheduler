c32fa9a60c15dee77aefad1b1a8d02eac3a35f7c
shlabisa <shlabisa@sarao.ac.za>
2025-07-18 13:42:48 +0200

    Added user comp, which contains router-outlet to support routing, and is a default component

---
+++ frontend/src/app/components/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+}
