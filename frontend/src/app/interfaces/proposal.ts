4399a299ad74366d806aafdfd0a66305b7c8f971
shlabisa <shlabisa@sarao.ac.za>
2025-07-18 13:41:20 +0200

    Added interface and model for proposal and timetable

--- frontend/src/app/interfaces/proposal.ts
+++ frontend/src/app/interfaces/proposal.ts
@@ -1,4 +1,3 @@
-import { Time } from "@angular/common";
 
 export interface Proposal {
     id: number;
@@ -9,13 +8,13 @@ export interface Proposal {
     instrument_integration_time: number;
     instrument_band: string;
     instrument_pool_resources: string;
-    lst_start_time: Time;
-    lst_start_end_time: Time;
+    lst_start_time: string;
+    lst_start_end_time: string;
     simulated_duration: number;
     night_obs: boolean;
     avoid_sunrise_sunset: boolean;
     minimum_antennas: number;
-    general_comments: string;
+    general_comments: string | null;
     prefered_dates_start_date: Date[];
     prefered_dates_end_date: Date[];
     avoid_dates_start_date: Date[];
