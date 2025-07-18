87a4c0c724e04cc29ddc31d95d10f682c8878c11
shlabisa <shlabisa@sarao.ac.za>
2025-07-18 13:49:01 +0200

    Added, then implemented structure and logic for timetables, details, generate timetable components

---
+++ frontend/src/app/components/edit-timetable/edit-timetable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-timetable',
+  imports: [],
+  templateUrl: './edit-timetable.component.html',
+  styleUrl: './edit-timetable.component.css'
+})
+export class EditTimetableComponent {
+
+}
---
+++ frontend/src/app/components/timetable-details/timetable-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-details',
+  imports: [],
+  templateUrl: './timetable-details.component.html',
+  styleUrl: './timetable-details.component.css'
+})
+export class TimetableDetailsComponent {
+
+}
