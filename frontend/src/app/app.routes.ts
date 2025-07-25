4e1df8cea8ea2b44357f35caec8952e36d46a9ca
shlabisa <shlabisa@sarao.ac.za>
2025-07-25 12:46:59 +0200

    Installed new packages, and commented out edit component on the router config

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { TimetablesComponent } from './components/timetables/timetables.component';
 import { TimetableDetailsComponent } from './components/timetable-details/timetable-details.component';
-import { EditTimetableComponent } from './components/edit-timetable/edit-timetable.component';
+//import { EditTimetableComponent } from './components/edit-timetable/edit-timetable.component';
 import { GenerateTimetableComponent } from './components/generate-timetable/generate-timetable.component';
+import { timetableGuard } from './guards/timetable.guard';
 
 export const routes: Routes = [
     {
@@ -15,11 +16,11 @@ export const routes: Routes = [
                 path: 'timetables', component: TimetablesComponent
             },
             {
-                path: 'timetables/:id/details', component: TimetableDetailsComponent
-            },
+                path: 'timetables/:id/details', component: TimetableDetailsComponent, canActivate: [timetableGuard]
+            },/*
             {
                 path: 'timetables/:id/edit', component: EditTimetableComponent
-            },
+            },*/
             {
                 path: 'timetables/generate', component: GenerateTimetableComponent
             },
