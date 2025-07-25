4c6348dd0c8278890a8d31981ce5c3ccc438b979
shlabisa <shlabisa@sarao.ac.za>
2025-07-25 12:39:12 +0200

    Removed router outlet from the user component

--- frontend/src/app/components/user/user.component.ts
+++ frontend/src/app/components/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   imports: [
-    RouterOutlet
+    NavbarComponent,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
